1ea6c7a4477d41027ff4a7c6e5ee10cc12dad5f2
felipin <felipinhobgouveia@gmail.com>
2024-11-22 14:28:31 -0300

    ex03, ex04

--- src/switchCase/Exercicio03.java
+++ src/switchCase/Exercicio03.java
@@ -9,7 +9,7 @@ public class Exercicio03 {
         int idade = 0;
 
         do {
-            System.out.println("Digite a sua idade,");
+            System.out.println("Digite a sua idade:");
             idade = sc.nextInt();
 
             if (idade < 0) {
@@ -33,6 +33,59 @@ public class Exercicio03 {
             case 12:
                 System.out.println("É uma criança");
                 break;
+            case 13:
+            case 14:
+            case 15:
+            case 16:
+            case 17:
+                System.out.println("É um adolescente");
+                break;
+            case 18:
+            case 19:
+            case 20:
+            case 21:
+            case 22:
+            case 23:
+            case 24:
+            case 25:
+            case 26:
+            case 27:
+            case 28:
+            case 29:
+            case 30:
+            case 31:
+            case 32:
+            case 33:
+            case 34:
+            case 35:
+            case 36:
+            case 37:
+            case 38:
+            case 39:
+            case 40:
+            case 41:
+            case 42:
+            case 43:
+            case 44:
+            case 45:
+            case 46:
+            case 47:
+            case 48:
+            case 49:
+            case 50:
+            case 51:
+            case 52:
+            case 53:
+            case 54:
+            case 55:
+            case 56:
+            case 57:
+            case 58:
+            case 59:
+                System.out.println("É um adulto");
+                break;
+            default:
+                System.out.println("É um idoso");
         }
 
         sc.close();
---
+++ src/switchCase/Exercicio04.java
@@ -0,0 +1,25 @@
+package switchCase;
+import java.util.Scanner;
+
+public class Exercicio04 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Escolha uma das 3 opções:\nA, B, C");
+        var opcao = sc.next().toLowerCase();
+
+        switch (opcao){
+            case "a":
+                System.out.println("Olá, mundo");
+                break;
+            case "b":
+                System.out.println("Bem vindo ao Java");
+                break;
+            case "c":
+                System.out.println("Sair do programa");
+                break;
+        }
+
+        sc.close();
+    }
+}
